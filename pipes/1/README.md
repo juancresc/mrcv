fab7a6cff05f2af5bdf9a2818da6a9095ff6298a
Juan Manuel Crescente <you@example.com>
2019-03-26 10:16:16 -0300

    dasdsa

---
+++ pipes/1/README.md
@@ -0,0 +1,6 @@
+-> Classic characterization of miRNA using shorstack
+-> Ordering and analyzing results
+-> RNASeq DATs estimation using DeSEQ2
+-> Target identification using psRNATarget and cDNAs
+-> Target validation using multiple degradomes approach
+-> Annotation of results, GO terms identification and different assemblies translations
--- pipes/1/pipe_7_degradome/1.1_run.sh
+++ pipes/1/pipe_7_degradome/1.1_run.sh
@@ -5,14 +5,10 @@ do
     #../../sw/sratoolkit.2.9.6-ubuntu64/bin/fastq-dump --gzip SRR1197127
     #files
     degradome=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}.fastq.gz
-    trimmed_degradome_gzipped=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed.fq.gz
-    qual_trimmed_degradome_gzipped=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed_qual.fq.gz
-    
-    trimmed_degradome_gzipped_name=${file_name}_trimmed.fq.gz
-    trimmed_degradome_report=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}.fq.gz_trimming_report.txt
-    trimmed_degradome_report_name=${file_name}.fastq.gz_trimming_report.txt
-    trimmed_degradome=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed.fq
-    fasta_degradome=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed.fasta
+    trimmed_degradome=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed.fastq.gz
+    trimmed_degradome_qual=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed_qual.fastq.gz
+    trimmed_degradome_qual_unzipped=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed_qual.fastq
+    fasta_degradome=/home/juan/Desktop/juan/bio/mrcv/data/degradome/${file_name}_trimmed_qual.fasta
 
     transcriptome=/home/juan/Desktop/juan/bio/mrcv/data/res/04_02_19/DEG-cdna.fasta
     miRNAs=/home/juan/Desktop/juan/bio/mrcv/data/res/04_02_19/miRNA.Y.DEG.fasta
@@ -28,24 +24,23 @@ do
     gsta_path=/home/juan/Desktop/juan/bio/mrcv/scripts/CleaveLand4/GSTAr_v1-0
     samtools_path=/home/juan/Desktop/juan/bio/sw/samtools-1.9
     bowtie_path=/home/juan/Desktop/juan/bio/sw/bowtie-1.2.2-linux-x86_64
+    trimmomatic_path=/home/juan/Desktop/juan/bio/sw/Trimmomatic-0.38
+    trim_galore_path=/home/juan/Desktop/juan/bio/sw/TrimGalore-0.6.0
+    fastqc_path=/home/juan/Desktop/juan/bio/sw/FastQC
+
     export PATH=$PATH:$bbduk_path
     export PATH=$PATH:$fastx_path
     export PATH=$PATH:$gsta_path
     export PATH=$PATH:$samtools_path
     export PATH=$PATH:$bowtie_path
-
-    trim_galore_path=/home/juan/Desktop/juan/bio/sw/TrimGalore-0.6.0
-    fastqc_path=/home/juan/Desktop/juan/bio/sw/FastQC
     export PATH=$PATH:$trim_galore_path
     export PATH=$PATH:$fastqc_path
+    export PATH=$PATH:$trimmomatic_path
 
     #trimming 
 
-    bbduk.sh in=$degradome out=$trimmed_degradome_gzipped ref=${$bbduk_path}/resources/adapters.fa ktrim=r k=23 mink=11 hdist=1 tpe tbo
-    bbduk.sh in=reads.fq out=clean.fq qtrim=r trimq=10
-
-
-
+    bbduk.sh in=$degradome out=$trimmed_degradome ref=${bbduk_path}/resources/adapters.fa ktrim=r k=23 mink=11 hdist=1 tpe tbo
+    bbduk.sh in=$trimmed_degradome out=$trimmed_degradome_qual qtrim=r trimq=10
 
     #trim_galore --small_rna $degradome
     # --length 17
@@ -55,7 +50,8 @@ do
     #gunzip $trimmed_degradome_gzipped $trimmed_degradome
 
     #fastq2fasta
-    fastq_to_fasta -i $trimmed_degradome -o $fasta_degradome -Q33
+    gunzip $trimmed_degradome_qual $trimmed_degradome_qual_unzipped
+    fastq_to_fasta -i $trimmed_degradome_qual_unzipped -o $fasta_degradome -Q33
 
     #running
     ./scripts/CleaveLand4/CleaveLand4.pl -e $fasta_degradome -u $miRNAs -n $transcriptome -t -p 0.05 -o $plots > $out_deg
