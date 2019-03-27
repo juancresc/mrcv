38a7f2f7c04aa9d9a3cfced78aa9aa0f57506133
Juan Manuel Crescente <you@example.com>
2019-03-27 08:44:01 -0300

    dasdsa

--- pipes/2/1.1_TEs_make.sh
+++
@@ -1,3 +0,0 @@
-cd /home/juan/Desktop/juan/bio/mrcv/data/TEs/db
-cat families_nr.fasta repbase.triticum.fasta trep-db_nr_Rel-16.fasta > TEs.fa
-blastn -evalue 10e-3  -query TEs.fa -db /home/juan/Desktop/juan/bio/data/IWGSC/42/blastdb/db -outfmt "6 qseqid sseqid qstart qend sstart send mismatch gaps pident evalue length qlen slen qcovs" > TEs.csv
