7e55bbd302d5eb2ec8ca800104540a2e0cb48f79
Juan Manuel Crescente <you@example.com>
2019-04-30 10:52:26 -0300

    smsAPI

--- pipes/3/all.sh
+++ pipes/3/all.sh
@@ -1,4 +1,14 @@
-./sw/ShortStack/ShortStack --readfile /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR1C.trimmed.fq.gz /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR3C.trimmed.fq.gz /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR1T.trimmed.fq.gz /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR3T.trimmed.fq.gz --genomefile /home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.dna.toplevel.fa --outdir data/ss/all_1000 --bowtie_m all --foldsize 1000
+#all sRNA and loci
+./sw/ShortStack/ShortStack --readfile /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR1C.trimmed.fq.gz /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR3C.trimmed.fq.gz /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR1T.trimmed.fq.gz /home/juan/Desktop/juan/bio/mrcv/data/sRNASeq/21dpiR3T.trimmed.fq.gz --genomefile /home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.dna.toplevel.fa --outdir data/ss/all_all --bowtie_m all --foldsize 1000
 
-reformat.sh in=data/ss/all_1000/merged_alignments.bam out=data/ss/all_1000/merged_alignments_21.bam  minlength=21 maxlength=21
+#filter bamfile to 21nt
+reformat.sh in=data/ss/all_all/merged_alignments.bam out=data/ss/all_all/merged_alignments_21.bam  minlength=21 maxlength=21
 
+#all loci and 21nt sRNA
+./sw/ShortStack/ShortStack --bamfile data/ss/all_all/merged_alignments_21.bam --genomefile /home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.dna.toplevel.fa --outdir data/ss/all_21nt --bowtie_m all --foldsize 1000
+
+#MITEs loci and all sRNA
+./sw/ShortStack/ShortStack --bamfile data/ss/all_all/merged_alignments.bam --genomefile /home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.dna.toplevel.fa --outdir data/ss/mite_all --bowtie_m all --foldsize 1000 --locifile /home/juan/Desktop/juan/bio/mrcv/data/genome/locifiles/mites.locifile.csv
+
+#MITEs loci and 21nt sRNA
+./sw/ShortStack/ShortStack --bamfile data/ss/all_all/merged_alignments_21.bam --genomefile /home/juan/Desktop/juan/bio/data/IWGSC/42/Triticum_aestivum.IWGSC.dna.toplevel.fa --outdir data/ss/mite_21 --bowtie_m all --foldsize 1000 --locifile /home/juan/Desktop/juan/bio/mrcv/data/genome/locifiles/mites.locifile.csv
