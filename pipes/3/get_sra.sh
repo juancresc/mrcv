965c8bb2f65ae9e5293c362312d060e9e8e5b520
Juan Manuel Crescente <you@example.com>
2019-05-07 16:39:37 -0300

    init

---
+++ pipes/3/get_sra.sh
@@ -0,0 +1,21 @@
+export PATH=$PATH:/home/juan/Desktop/juan/bio/sw/sratoolkit.2.9.6-ubuntu64/bin
+
+fastq-dump --gzip SRR6328644
+fastq-dump --gzip SRR6328645
+fastq-dump --gzip SRR6328646
+fastq-dump --gzip SRR6328648
+fastq-dump --gzip SRR6328647
+fastq-dump --gzip SRR6328649
+fastq-dump --gzip SRR6328650
+fastq-dump --gzip SRR6328651
+fastq-dump --gzip SRR6328652
+
+#SR4A(SRR6328644)
+#JNA(SRR6328645)
+#SR3C(SRR6328646)
+#JNS(SRR6328648)
+#JNC(SRR6328647)
+#SR4C(SRR6328649)
+#SR4S(SRR6328650)
+#SR3S(SRR6328651)
+#SR3A(SRR6328652)
